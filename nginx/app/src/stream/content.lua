579b49b39b55a572c3b831cdb4bcd5a3f01a31c5
libo.huang <huanglibo2010@gmail.com>
2017-11-28 18:02:39 +0800

    add log info

--- nginx/app/src/stream/content.lua
+++ nginx/app/src/stream/content.lua
@@ -1,8 +1,12 @@
 -- Copyright (C) 2017 Libo Huang (huangnauh), UPYUN Inc.
 local mload     = require "resty.load"
+local log       = ngx.log
+local INFO      = ngx.INFO
 
 local skey = "content" .. ngx.var.server_port
 local func = mload.load_script("script." .. skey, { global=true })
 if func then
     func()
+else
+    log(INFO, "script not exist, " .. skey)
 end
